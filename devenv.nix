300bfdcbb00144f7997ba1caee525f93e0db1c90
SymphonyIceAttack <1994370030@qq.com>
2025-11-26 08:58:28 +0800

    feat: claude init

---
+++ devenv.nix
@@ -0,0 +1,26 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  dotenv = {
+    disableHint = true;
+    enable = true;
+    filename = ".env.claude";
+  };
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  tasks."biome:check" = {
+    exec = "npx biome check --write";
+    before = [ "devenv:enterShell" ];
+  };
+
+}
